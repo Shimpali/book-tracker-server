f63aa9d8fb57b40c717c2a8d28412d18a8b587b4
Shimpali Deshpande <shimpali.deshpande@yahoo.com>
2022-05-05 15:09:37 +0530

    Removed typeorm

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,5 +1,4 @@
 import * as bcrypt from 'bcrypt';
-import { Connection, getConnection, getConnectionOptions } from 'typeorm';
 
 export const toPromise = <T>(data: T): Promise<T> => {
   return new Promise<T>((resolve) => {
@@ -7,32 +6,32 @@ export const toPromise = <T>(data: T): Promise<T> => {
   });
 };
 
-export const getDbConnectionOptions = async (connectionName = 'default') => {
-  const options = await getConnectionOptions(
-    process.env.NODE_ENV || 'development'
-  );
-  return {
-    ...options,
-    name: connectionName,
-  };
-};
+// export const getDbConnectionOptions = async (connectionName = 'default') => {
+//   const options = await getConnectionOptions(
+//     process.env.NODE_ENV || 'development'
+//   );
+//   return {
+//     ...options,
+//     name: connectionName,
+//   };
+// };
 
-export const getDbConnection = async (
-  connectionName = 'default'
-): Promise<Connection> => {
-  return await getConnection(connectionName);
-};
+// export const getDbConnection = async (
+//   connectionName = 'default'
+// ): Promise<Connection> => {
+//   return await getConnection(connectionName);
+// };
 
-export const runDbMigrations = async (
-  connectionName = 'default'
-): Promise<void> => {
-  const conn = await getDbConnection(connectionName);
-  await conn.runMigrations();
-};
+// export const runDbMigrations = async (
+//   connectionName = 'default'
+// ): Promise<void> => {
+//   const conn = await getDbConnection(connectionName);
+//   await conn.runMigrations();
+// };
 
 export const comparePasswords = async (
   userPassword,
-  currentPassword
+  currentPassword,
 ): Promise<boolean> => {
   return await bcrypt.compare(currentPassword, userPassword);
 };
