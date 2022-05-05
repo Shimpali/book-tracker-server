b618e435f0b8db0258650154766cd4a25aaffd02
Shimpali Deshpande <shimpali.deshpande@yahoo.com>
2022-05-05 15:05:43 +0530

    Home Route fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHomeRoute(): string {
+    return 'Hello. Go to /api to access the API.';
   }
 }
