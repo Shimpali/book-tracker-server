b7d7005a5136baa6fcbe2c034b9b9a83ac99d905
Shimpali Deshpande <shimpali.deshpande@yahoo.com>
2022-06-22 20:10:50 +0530

    CORS fix

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors({
-    origin: ['*'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
+  app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
